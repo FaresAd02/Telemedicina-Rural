e28cc68a480f3bff7df7fdf91515e5cb6f4c850c
FaresAd02 <farescastro02@icloud.com>
2025-07-26 11:25:52 -0600

    Cambios en Firebase

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 // Configuración Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyB25RJkWpFOxihefSN8G8BgjBkp1DaJgCo",
-    authDomain: "telemedicina-rural.firebaseapp.com",
-    projectId: "telemedicina-rural",
-    storageBucket: "telemedicina-rural.firebasestorage.app",
-    messagingSenderId: "545941808760",
-    appId: "1:545941808760:web:312ca5a2d010a8d9a1ef8d",
-    measurementId: "G-M5LBNG6G21"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Variables globales
